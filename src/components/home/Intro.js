9d72436500136924865964409a5c8c709f8309a2
unknown <muhammedsadeed70@gmail.com>
2022-06-07 20:24:51 +0400

    About section animations

--- src/components/home/Intro.js
+++ src/components/home/Intro.js
@@ -23,7 +23,7 @@ function Intro() {
 </p> </motion.div>
 
 
-<motion.div className='pic' initial={{x:'100%'}} animate={{x:'0%'}} transition={{delay:'1.6', duration:'0.3', type:'spring',dampness:'100'}}><img src={blob} alt='hero section image'></img></motion.div>
+<motion.div className='pic' initial={{x:'100%'}} animate={{x:'0%'}} transition={{delay:'1.6', duration:'0.6', type:'spring'}}><img src={blob} alt='hero section image'></img></motion.div>
 </div>
   )
 }
