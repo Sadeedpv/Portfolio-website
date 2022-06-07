e7ba3369d755fcd7d3521a2d8c31654d5dc18495
unknown <muhammedsadeed70@gmail.com>
2022-06-07 10:57:03 +0400

    typing effect

--- src/components/home/Intro.js
+++ src/components/home/Intro.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './intro.css'
 import blob from './blob.png'
 import { motion } from 'framer-motion'
+import TypeAnimation from 'react-type-animation';
 
 function Intro() {
   return (
+    
     <div className='grid'>
     <motion.div className='intro'>
         <h1><motion.span className='hey'  initial={{opacity:'0%'}} animate={{opacity:'100%'}} transition={{delay:'0.8', duration:'2'}}>Hello,</motion.span> <br />
-         I'm <motion.span className='name'  initial={{opacity:'0%'}} animate={{opacity:'100%'}} transition={{delay:'1.6', duration:'2'}}>&#60;Mohd Sadeed /&#62;</motion.span></h1>
+         I'm <motion.span className='name'  initial={{opacity:'0%'}} animate={{opacity:'100%'}} transition={{delay:'1.2', duration:'2'}} exit={{opacity:'0%', y:'200%'}}
+         
+         >&#60;<TypeAnimation cursor={true} speed={10000} sequence={['Mohd Sadeed', 3000, '', 1000, 'A Developer', 3000, '', 3000]} repeat={Infinity} className='block'></TypeAnimation>  /&#62;</motion.span></h1>
         <motion.p className='gray-text' initial={{opacity:'0%'}} animate={{opacity:'50%'}} transition={{delay:'2', duration:'2'}}>I am a developing developer. I would love to help you with your next big project. Contact me for all kinds of collaborations</motion.p>
         <button className='intro-button'>Contact me</button>
         <br></br><p className='nomore'>Know more <svg width="28" height="20" viewBox="0 0 28 20" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -19,7 +23,7 @@ function Intro() {
 </p> </motion.div>
 
 
-<motion.div className='pic' initial={{x:'100%'}} animate={{x:'0%'}} transition={{delay:'0.5', duration:'1.2', type:'spring',damping:'30', stiffness:'100'}}><img src={blob}></img></motion.div>
+<motion.div className='pic' initial={{x:'100%'}} animate={{x:'0%'}} transition={{delay:'1.6', duration:'1.2', type:'spring',damping:'30', stiffness:'100'}}><img src={blob}></img></motion.div>
 </div>
   )
 }
