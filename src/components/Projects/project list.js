42d2d4599d801585972ff77048fe6ed8225d556c
unknown <muhammedsadeed70@gmail.com>
2022-07-12 22:54:34 +0400

    Update projects

--- src/components/Projects/project list.js
+++ src/components/Projects/project list.js
@@ -16,15 +16,31 @@ const projectlist = [{
 },
 {
     id: 3,
-    name:'Portfolio website',
-    description: 'This is a responsive portfolio website I made with pure html css and javascript',
-    techstack: 'Html, css, javascript, bootstrap'
+    name:'Spotify clone',
+    description: 'A spotify UI clone which allows users to signin and access their playlist',
+    techstack: 'Reactjs and Spotify API was used to fetch the data from the API',
 },
 {
     id: 4,
     name:'Flask Authentication',
     description: 'This is a flask application which allows users to signup and signin. It handles the user authentication and the user data. It is a microservice application with the additional features of resetting password and sending mails',
     techstack: 'I used Flask framework for the application and used the flask-wtf library for the form validation. I made use of the  flask-mail library for sending emails and flask-bcrypt library for the password encryption. Sqlite was used as the database',
+    link:'',
+    img:''
+},
+{
+    id: 5,
+    name:'Firebase-CRUD',
+    description: 'A basic firebase application which makes use of Firebase CRUD operations.',
+    techstack: 'I used Firebase and React for the application and used the react-firebase-hook library for the authentication. I made use of the firebase-cloud database library for the CRUD operations',
+    link:'',
+    img:''
+},
+{
+    id: 6,
+    name:'News-API',
+    description: 'A news application which makes use of the News API to fetch the news from the API. With this app, you can search for trending news based on particular topic',
+    techstack: 'I used the News API for the application with React as my front-end and I made use of the react-router-dom library for the routing',
     link:'',
     img:''
 }]
--- src/components/plans/Card.js
+++ src/components/plans/Card.js
@@ -7,19 +7,19 @@ function Card() {
     <div className='plans'>
         <div className='card-head'>Pricing</div>
     <div className='card-grid'>
-        <CardItem size='large' plan='Basic' money='10,000' delay='0.4'
+        <CardItem size='large' plan='Basic' money='5,000' delay='0.4'
         a1='A static single page website'
         a2='HTML,CSS and JAVASCRIPT'
         a3='Free hosting on Github'
         a4='No backend'/>
 
-        <CardItem size='normal' plan='Standard' money='15,000' delay='0.6'
+        <CardItem size='normal' plan='Standard' money='10,000' delay='0.6'
         a1='Multipage websites'
         a2='Stunning visuals and animations'
         a3='Free hosting on Github'
         a4='No backend'/>
 
-        <CardItem size='normal' plan='Premium' money='25,000' delay='0.8'
+        <CardItem size='normal' plan='Premium' money='15,000' delay='0.8'
         a1='Full stack web applications'
         a2='Mobile applications'
         a3='No free hosting'
